9b6f77ba263980fd39171235a92fd623f489e327
toxa14147 <kaftanov1212@mail.ru>
2017-04-18 23:12:21 +0300

    муки

---
+++ snake_mine_app/snake_mine_app/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake_mine_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//размер окна
+            Console.SetBufferSize(80, 85);
+            //стартовая точка р
+            point p = new point(1,3,'*');
+        }
+    }
+}
---
+++ snake_mine_app/snake_mine_app/point.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake_mine_app
+{
+    class point
+    { //координаты
+        public int x;
+        public int y;
+        public char sym;
+        //задаём точку с координатами х у z
+        public point(int _x, int _y, char _sym) {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+        //задаём перемещение точке
+        public point(point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+        //рисуем точку
+        public void Drow()
+        {
+            Console.SetWindowPosition(x, y);
+            Console.Write(sym);
+        }
+        //зачищаем точки
+        public void Clear()
+        {
+            char sym = ' ';
+            Console.Write(sym);
+        }
+    }
+    
+}
