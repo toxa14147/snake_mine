33bc9e1200258f2a23416b4a0d58516238208283
toxa14147 <kaftanov1212@mail.ru>
2017-04-24 20:49:49 +0300

    оперативка!

--- snake_mine_app/snake_mine_app/point.cs
+++ snake_mine_app/snake_mine_app/point.cs
@@ -11,17 +11,17 @@ namespace snake_mine_app
         public int x;
         public int y;
         public char sym;
-        public Point (int _x, int _y, char _sym)
+        public Point(int _x, int _y, char _sym)
         {
             x = _x;
             y = _y;
             sym = _sym;
         }
-        public void Draw ()
+        public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
         }
-        
+
     }
 }
